7ad54d42d01f70314ef762039982d7045670a758
Kenny-star <54686876+Kenny-star@users.noreply.github.com>
2021-09-02 23:10:35 -0400

    Add project files.

---
+++ TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Instantiates the ViewBag dictionary tip values to 0 as a starting point
+            ViewBag.tip15 = 0;
+            ViewBag.tip20 = 0;
+            ViewBag.tip25 = 0;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            //If values were successfully binded to the TipCalculatorModel from the request in the view, then execute the tips calculation functions.
+            if (ModelState.IsValid)
+            {
+                ViewBag.tip15 = model.Calculate15Tip();
+                ViewBag.tip20 = model.Calculate20Tip();
+                ViewBag.tip25 = model.Calculate25Tip();
+            }
+            else
+            {
+            
+                ViewBag.tip15 = 0;
+                ViewBag.tip20 = 0;
+                ViewBag.tip25 = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ TipCalculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        //Simple error validation output for the cost of meal, 
+        [Required (ErrorMessage = "Please input a cost of meal.")]
+        [Range(1,int.MaxValue, ErrorMessage ="Please input a positive cost of meal.")]
+        public decimal? COM { get; set; }
+
+        //Cost of meal 15%-20%-25% tip functions.
+        public decimal? Calculate15Tip()
+        {
+            decimal? tip1 = COM * (decimal)1.15;
+            return tip1;
+        }
+
+        public decimal? Calculate20Tip()
+        {
+            decimal? tip2 = COM * (decimal)1.20;
+            return tip2;
+        }
+        public decimal? Calculate25Tip()
+        {
+            decimal? tip3 = COM * (decimal)1.25;
+            return tip3;
+        }
+    }
+}
